9f1b4f79f1a01810e1bee4f1e86019b6ca9039f8
davis-jordan <32600362+davis-jordan@users.noreply.github.com>
2023-02-05 14:51:45 -0600

    removed rallycoding dependency as it is outdated

--- index.js
+++ index.js
@@ -1,53 +1,75 @@
 module.exports = {
-  extends: [
-    'rallycoding',
-    'eslint:recommended',
-    'plugin:react/recommended',
-  ],
-  rules: {
-    semi: ['error', 'always'],
-    'no-console': ['error', { allow: ['warn', 'log'] }],
-    'react/require-extension': 'off',
-    'arrow-body-style': 0,
-    'no-else-return': 0,
-    'no-unused-expressions': 0,
-    'brace-style': [1, 'stroustrup'],
-    'max-len': [1, 300],
-    'prefer-template': 0,
-    camelcase: ['error', {
-      allow: [
-        'UNSAFE_componentWillMount',
-        'UNSAFE_componentWillUpdate',
-        'UNSAFE_componentWillReceiveProps',
-      ]
-    }],
-    'react/prop-types': 0,
-    'react/sort-comp': [1, {
-      order: [
-        'instance-variables',
-        'lifecycle',
-        '/^on.+$/',
-        'render',
-        'everything-else',
-        'static-methods',
-      ]
-    }]
+  env: {
+    browser: true,
+    es2021: true
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
-        moduleDirectory: ['node_modules', 'src/'],
-      }
-    },
+  extends: [
+    'plugin:react/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   plugins: [
-    'import',
-    'jest',
-    'typescript'
+    'react'
   ],
-  env: {
-    browser: true
-  },
-  parser: 'babel-eslint'
-};
+  rules: {
+  }
+}
+
+// module.exports = {
+//   extends: [
+//     'rallycoding',
+//     'eslint:recommended',
+//     'plugin:react/recommended',
+//   ],
+//   rules: {
+//     semi: ['error', 'always'],
+//     'no-console': ['error', { allow: ['warn', 'log'] }],
+//     'react/require-extension': 'off',
+//     'arrow-body-style': 0,
+//     'no-else-return': 0,
+//     'no-unused-expressions': 0,
+//     'brace-style': [1, 'stroustrup'],
+//     'max-len': [1, 300],
+//     'prefer-template': 0,
+//     camelcase: ['error', {
+//       allow: [
+//         'UNSAFE_componentWillMount',
+//         'UNSAFE_componentWillUpdate',
+//         'UNSAFE_componentWillReceiveProps',
+//       ]
+//     }],
+//     'react/prop-types': 0,
+//     'react/sort-comp': [1, {
+//       order: [
+//         'instance-variables',
+//         'lifecycle',
+//         '/^on.+$/',
+//         'render',
+//         'everything-else',
+//         'static-methods',
+//       ]
+//     }]
+//   },
+//   settings: {
+//     'import/resolver': {
+//       node: {
+//         extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+//         moduleDirectory: ['node_modules', 'src/'],
+//       }
+//     },
+//   },
+//   plugins: [
+//     'import',
+//     'jest',
+//     'typescript'
+//   ],
+//   env: {
+//     browser: true
+//   },
+//   parser: 'babel-eslint'
+// };
