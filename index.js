37f11f4920324d560f505d2a2874679127b7b0eb
davis-jordan <32600362+davis-jordan@users.noreply.github.com>
2023-02-05 12:35:52 -0600

    Initialied eslint rules

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
 module.exports = {
-
-  globals: {
-    MyGlobal: true
-  },
-
+  extends: [
+    'rallycoding',
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
   rules: {
-    semi: [2, "always"]
-  }
-
+    semi: ['error', 'always'],
+    'no-console': ['error', { allow: ['warn', 'log'] }],
+    'react/require-extension': 'off',
+    'arrow-body-style': 0,
+    'no-else-return': 0,
+    'no-unused-expressions': 0,
+    'brace-style': [1, 'stroustrup'],
+    'max-len': [1, 300],
+    'prefer-template': 0,
+    camelcase: ['error', {
+      allow: [
+        'UNSAFE_componentWillMount',
+        'UNSAFE_componentWillUpdate',
+        'UNSAFE_componentWillReceiveProps',
+      ]
+    }],
+    'react/prop-types': 0,
+    'react/sort-comp': [1, {
+      order: [
+        'instance-variables',
+        'lifecycle',
+        '/^on.+$/',
+        'render',
+        'everything-else',
+        'static-methods',
+      ]
+    }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+        moduleDirectory: ['node_modules', 'src/'],
+      }
+    },
+  },
+  plugins: [
+    'import',
+    'typescript'
+  ],
+  env: {
+    browser: true
+  },
+  parser: 'babel-eslint'
 };
